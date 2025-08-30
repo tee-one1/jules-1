af54de52da6416784c070ba45a2e92bd73587271
Mussi <140247381+tee-one1@users.noreply.github.com>
2025-08-30 00:19:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function getWeatherData() {
   // Istanbul coordinates
   const lat = 41.01;
   const lon = 28.98;
-  const apiKey = 'YOUR_API_KEY'; // Placeholder for the API key
+  const apiKey = '37477681ad8fc75255637d5c7bc348e2'; // Placeholder for the API key
   const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
   try {
